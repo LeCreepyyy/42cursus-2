68a8e2fa4735db994b0d6f0c6f20808c861a0e96
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-10-02 15:51:18 +0200

    CPP00

--- CPP/cpp00/ex01/main.cpp
+++ CPP/cpp00/ex01/main.cpp
@@ -1,31 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/02 10:09:44 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/10/02 15:23:00 by vpoirot          ###   ########.fr       */
+/*   Created: 2023/10/02 14:51:51 by vpoirot           #+#    #+#             */
+/*   Updated: 2023/10/02 15:50:38 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <cctype>
 
-int	main(int argc, char **argv) {
-	if (argc <= 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else {
-		std::string res;
-		for (int i = 1; i < argc; i++) {
-			res.append(argv[i]);
-		}
-		for (std::string::iterator it = res.begin(); it != res.end(); it++) {
-			*it = std::toupper(*it);
-		}
-		std::cout << res << std::endl;
+
+#include <iostream>
+
+int	main(void) {
+	std::cout << "Send ADD for add a new contact" << std::endl;
+	std::cout << "Send SEARCH for display your contact" << std::endl;
+	std::cout << "Send EXIT for quit" << std::endl;
+
+	std::string buffer;
+	std::cin >> buffer;
+	for (; buffer != "EXIT" && !std::cin.eof(); std::cin >> buffer) {
+		if (buffer != "ADD" && buffer != "SEARCH")
+			std::cout << "Wrong command" << std::endl;
 	}
+	std::cout << "Good Bye !" << std::endl;
 	return (0);
 }
