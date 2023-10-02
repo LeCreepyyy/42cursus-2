17ff83fba667580667c5b8ffd3f5e7dfde7058ef
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-10-02 14:20:34 +0200

    cpp00

--- CPP/cpp00/ex00/megaphone.cpp
+++ CPP/cpp00/ex00/megaphone.cpp
@@ -6,7 +6,27 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:09:44 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/10/02 10:09:50 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/10/02 13:52:26 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int	main(int argc, char **argv) {
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		std::string res;
+		for (int i = 1; i < argc; i++) {
+			res.append(argv[i]);
+		}
+		for (std::string::iterator it = res.begin(); it != res.end(); it++) {
+			*it = std::toupper(*it);
+		}
+		std::cout << res << std::endl;
+	}
+	return (0);
+}
