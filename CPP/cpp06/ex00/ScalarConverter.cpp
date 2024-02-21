0683d470d3bd60010958e21f0e0a73a906617b16
Vincent Poirot <vpoirot@k1r3p7.42mulhouse.fr>
2024-02-21 15:25:46 +0100

    cpp06

--- CPP/cpp06/ex00/ScalarConverter.cpp
+++ CPP/cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:16:14 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/02/21 15:13:13 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/02/21 15:25:15 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 bool findNbr(const std::string& arg) {
     int isPoint = 0;
     for (size_t i = 0; i < arg.length(); ++i) {
-        if (i == arg.length() - 1 && arg[i] == 'f')
+        if (i == arg.length() - 1 && arg[i] == 'f' && isPoint == 1)
             continue;
+        if (i == arg.length() - 1 && arg[i] == '.')
+            return (false);
         if (arg[i] != '.' && (arg[i] < '0' || arg[i] > '9'))
             return (false);
         if (arg[i] == '.') {
@@ -31,9 +33,9 @@ bool findNbr(const std::string& arg) {
 int    getType(std::string arg) {
     if (arg.size() == 1 && (arg[0] < '0' || arg[0] > '9'))
         return (_CHAR);
-    if (findNbr(arg) == true)
+    if (findNbr(arg) == true && arg.find('.') == std::string::npos)
         return (_INT);
-    if (findNbr(arg) == true)
+    if (findNbr(arg) == true && arg.find('f') != std::string::npos)
         return (_FLOAT);
     if (findNbr(arg) == true)
         return (_DOUBLE);
