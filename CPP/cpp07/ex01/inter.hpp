7960f7bd7d657eb3eebd4f38f0eebc5694338354
Vincent Poirot <vpoirot@k1r3p5.42mulhouse.fr>
2024-02-27 14:28:42 +0100

    cpp07

--- CPP/cpp07/ex01/inter.hpp
+++ CPP/cpp07/ex01/inter.hpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:12:33 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/02/27 14:27:25 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:28:37 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define INTER_HPP
 
 template<typename A>
-void    iter(A* array, int len, void(*f)(A a)) {
+void    iter(A* array, unsigned int len, void(*f)(A a)) {
     while (len >= 0) {
         f(array[len]);
         len--;
