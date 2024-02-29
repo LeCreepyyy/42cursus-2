72b9a84169e1a48a3e97a01247a6ef992845c549
Vincent Poirot <vpoirot@k1r3p2.42mulhouse.fr>
2024-02-29 10:11:09 +0100

    cpp07

--- CPP/cpp07/ex01/inter.hpp
+++ CPP/cpp07/ex01/inter.hpp
@@ -6,18 +6,20 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:12:33 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/02/27 14:28:37 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/02/29 09:59:41 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTER_HPP
 # define INTER_HPP
 
+# include <string>
+# include <iostream>
+
 template<typename A>
-void    iter(A* array, unsigned int len, void(*f)(A a)) {
-    while (len >= 0) {
+void    iter(A* array, int len, void(*f)(A a)) {
+    while (--len >= 0) {
         f(array[len]);
-        len--;
     }
 }
 
--- CPP/cpp07/ex01/main.cpp
+++ CPP/cpp07/ex01/main.cpp
@@ -6,12 +6,21 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:12:15 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/02/27 14:12:31 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/02/29 10:10:52 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inter.hpp"
 
+template<typename B>
+void    printArrayValue(B b) {
+    std::cout << b << std::endl;
+}
+
 int main(void) {
-    ;
+    int arrayInt[5] = {14, 12, 19, 66, 99};
+    iter(arrayInt, 5, printArrayValue);
+    
+    std::string arrayString[3] = {"Cars", "Toy Story", "Nemo"};
+    iter(arrayString, 3, printArrayValue);
 }
