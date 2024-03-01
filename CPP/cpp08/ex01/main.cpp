087d28293cf205ae33ce5c02105fb00df2ede5d7
Vincent Poirot <vpoirot@k1r3p3.42mulhouse.fr>
2024-03-01 12:25:29 +0100

    cpp08

--- CPP/cpp08/ex01/main.cpp
+++ CPP/cpp08/ex01/main.cpp
@@ -6,15 +6,15 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 10:49:15 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/03/01 11:37:26 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/03/01 12:25:13 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
 int main(void) {
-    Span sp = Span(10000);
-    for (int i = 0; i < 10000; i++) {
+    Span sp = Span(100000);
+    for (int i = 0; i < 100000; i++) {
         sp.addNumber(i);
     }
 
