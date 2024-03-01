34d439512e84108a687cf72ae0cbe12d6fd9b627
Vincent Poirot <vpoirot@k1r3p3.42mulhouse.fr>
2024-03-01 13:51:07 +0100

    cpp08

--- CPP/cpp08/ex01/main.cpp
+++ CPP/cpp08/ex01/main.cpp
@@ -6,20 +6,26 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 10:49:15 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/03/01 12:25:13 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/03/01 13:50:38 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
 int main(void) {
-    Span sp = Span(100000);
-    for (int i = 0; i < 100000; i++) {
-        sp.addNumber(i);
-    }
+    // Span sp = Span(100000);
+    // for (int i = 0; i < 100000; i++) {
+    //     sp.addNumber(i);
+    // }
+
+    Span sp = Span(5);
+    sp.addNumber(15);
+    sp.addNumber(19);
+    sp.addNumber(48);
+    sp.addNumber(999);
+    sp.addNumber(15);
 
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
-
     return 0;
 }
