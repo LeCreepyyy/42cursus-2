f60fbc3358cf8c450103983c92ee923a96b5bb41
Vincent Poirot <vpoirot@k1r3p3.42mulhouse.fr>
2024-03-11 09:50:22 +0100

    cpp08

--- CPP/cpp08/ex02/main.cpp
+++ CPP/cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:54:24 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/03/08 14:22:14 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/03/11 09:49:52 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
@@ -34,6 +33,6 @@ int main()
         std::cout << *it << std::endl;
         ++it;
     }
-    //std::stack<int> s(mstack);
+    // std::stack<int> s(mstack);
     return 0;
 }
