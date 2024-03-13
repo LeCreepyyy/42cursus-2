48998fad90847f28f22351db62def96470da24a0
Vincent Poirot <vpoirot@k1r3p3.42mulhouse.fr>
2024-03-13 11:57:16 +0100

    cpp09

--- CPP/cpp09/ex00/main.cpp
+++ CPP/cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 10:45:55 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/03/13 11:50:37 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/03/13 11:56:42 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 bool parsing(std::string data) {
     size_t i = 0;
-    static int pass = 2;
-    std::cout << pass++ << " : ";
+    static int pass = 1;
+    std::cout << ++pass << " : ";
     for (; i != data.size(); ++i) {
         if (data[i] && ((data[i] >= '0' && data[i] <= '9') || data[i] == ',' || data[i] == '-' || data[i] == '.'))
             continue ;
@@ -28,6 +28,14 @@ bool parsing(std::string data) {
     }
     if ((data[5] != '0' && data[5] != '1')
         || (data[8] != '0' && data[8] != '1' && data[8] != '2' && data[8] != '3')) {
+        std::cout << "Error: bad input => \"" << data << "\"" << std::endl;
+        return (false);
+    }
+    if ((data[5] == '1' && (data[6] > '2' || data[6] < '0'))) {
+        std::cout << "Error: bad input => \"" << data << "\"" << std::endl;
+        return (false);
+    }
+    if ((data[8] == '3' && (data[9] > '1' || data[9] < '0'))) {
         std::cout << "Error: bad input => \"" << data << "\"" << std::endl;
         return (false);
     }
