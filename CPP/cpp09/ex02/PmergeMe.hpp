1c47ea3bcb481858535d0c7cb036314b9c258430
Vincent Poirot <vpoirot@k1r2p3.42mulhouse.fr>
2024-03-26 14:28:15 +0100

    cpp09

---
+++ CPP/cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 10:30:42 by vpoirot           #+#    #+#             */
+/*   Updated: 2024/03/26 14:14:24 by vpoirot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGE_HPP
+# define PMERGE_HPP
+
+# include <iostream>
+# include <string>
+# include <deque>
+# include <vector>
+# include <cstdlib>
+
+#endif
---
+++ CPP/cpp09/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 10:17:17 by vpoirot           #+#    #+#             */
+/*   Updated: 2024/03/26 14:26:57 by vpoirot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int main(int argc, char** argv) {
+    // bool parsing : only numbers / no doubles / only int / no negative / int max
+    std::deque<int> d1;
+    std::deque<int> d2;
+    for (int i = 1; i < argc; i += 2) {
+        if (i + 1 == argc)
+            d1.push_back(atoi(argv[i]));
+        else if (atoi(argv[i]) > atoi(argv[i + 1])) {
+            d1.push_back(atoi(argv[i]));
+            d2.push_back(atoi(argv[i + 1]));
+        }
+        else if (atoi(argv[i]) < atoi(argv[i + 1])) {
+            d1.push_back(atoi(argv[i + 1]));
+            d2.push_back(atoi(argv[i]));
+        }
+    }
+}
