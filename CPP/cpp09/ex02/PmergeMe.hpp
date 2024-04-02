41887dd66db9d92c3c927fa9405b13b55c28eca9
Vincent Poirot <vpoirot@k1r2p3.42mulhouse.fr>
2024-04-02 14:27:13 +0200

    cpp09

--- CPP/cpp09/ex02/PmergeMe.hpp
+++ CPP/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 10:30:42 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/04/02 11:43:19 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/04/02 14:26:48 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,11 @@ template<typename T, class Container = std::deque<T> >
 void sort(Container o1, Container o2, std::vector<int> jb) {
     std::deque<Container> d1;
     std::deque<Container> d2;
+    // creation de paire
+    // condition pour rester dans la recursivite
     sort(d1, d2, jb);
+    // insert sort avec jb
+    //print le resultat
 }
 
 #endif
