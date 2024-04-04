5dc2ba51e29eafaa0599aaab3a1f9a28a27a2d70
Vincent Poirot <vpoirot@k1r2p3.42mulhouse.fr>
2024-04-04 14:23:54 +0200

    cpp09

--- CPP/cpp09/ex02/PmergeMe.hpp
+++ CPP/cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 10:30:42 by vpoirot           #+#    #+#             */
-/*   Updated: 2024/04/03 14:04:40 by vpoirot          ###   ########.fr       */
+/*   Updated: 2024/04/04 14:23:43 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,29 @@ bool parsing(int argc, char** argv);
 std::vector<int> getJacobsthal(int size);
 
 template<typename T>
-void sort(T o1, T o2, std::vector<int> jb) {
-    std::deque<T, T> d1;
-    std::deque<T, T> d2;
+void sort(std::deque<T> o1, std::deque<T> o2, std::vector<int> jb) {
+    std::deque<std::deque<T>> d1;
+    std::deque<std::deque<T>> d2;
     (void)o1;
     (void)o2;
     (void)jb;
     // creation de paire
-    for () {
-    }
     // condition pour rester dans la recursivite
     // -sort(d1, d2, jb);
     // insert sort avec jb
     // print le resultat
 }
+
+// template<typename T>
+// std::deque<std::deque<T> > nestDeque(const std::deque<T>& inputDeque, int depth) {
+//     if (depth <= 1) {
+//         // Si la profondeur est inférieure ou égale à 1, retourne la deque originale
+//         return std::deque<std::deque<T> >(1, inputDeque);
+//     } else {
+//         // Sinon, appelle récursivement la fonction avec une profondeur réduite
+//         std::deque<std::deque<T> > nestedDeque(1, inputDeque);
+//         return nestDeque(nestedDeque, depth - 1);
+//     }
+// }
 
 #endif
